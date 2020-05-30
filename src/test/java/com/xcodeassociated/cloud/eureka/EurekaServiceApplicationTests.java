6b7906e74735dc861573944fb3a9253260e9cf6a
Janusz Majchrzak <janusz.majchrzak@student.uj.edu.pl>
2020-05-30 13:28:17 +0200

    Initial commit

---
+++ src/test/java/com/xcodeassociated/cloud/eureka/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcodeassociated.cloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
